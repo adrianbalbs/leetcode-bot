1540354b118b38cb3cf40fa952c346b59691746b
adrianbalbs <adrianbalbs@hotmail.com>
2025-06-30 20:08:35 +1000

    initial scraper impl

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-rod/rod"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	page := rod.New().MustConnect().MustPage("https://neetcode.io/practice?tab=neetcode150")
+	page.MustElement(`button.navbar-btn.is-rounded[data-tooltip="Show List View"]`).MustClick()
+	tables := page.MustElements("table > tbody")
+	for _, table := range tables {
+		rows := table.MustElements("tr")
+		for _, row := range rows {
+			rowChild := row.MustElements("td")[2]
+			title, err := rowChild.MustElement("a.table-text.text-color").Text()
+			if err != nil {
+				fmt.Println("No Title")
+				return
+			}
+			link := rowChild.MustElement("a.has-tooltip-bottom").MustAttribute("href")
+			fmt.Println(title, ":", *link)
+		}
+
+	}
 }
