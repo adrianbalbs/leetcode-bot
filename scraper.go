101ce88f4e44c5fc34f3345e7418d965ba4c7340
adrianbalbs <adrianbalbs@hotmail.com>
2025-08-23 15:45:09 +1000

    basic neetcode 150 scraper

--- scraper.go
+++ scraper.go
@@ -6,22 +6,33 @@ import (
 	"github.com/go-rod/rod"
 )
 
+type LeetcodeLink struct {
+	name string
+	url  string
+}
+
+// TODO: Move this into a package
+func problem(titleSlug string) {
+}
+
 func main() {
 	page := rod.New().MustConnect().MustPage("https://neetcode.io/practice?tab=neetcode150")
 	page.MustElement(`button.navbar-btn.is-rounded[data-tooltip="Show List View"]`).MustClick()
 	tables := page.MustElements("table > tbody")
+
+	// leetcodeLinks := []LeetcodeLink{}
 	for _, table := range tables {
-		rows := table.MustElements("tr")
-		for _, row := range rows {
+		for _, row := range table.MustElements("tr") {
 			rowChild := row.MustElements("td")[2]
-			title, err := rowChild.MustElement("a.table-text.text-color").Text()
+			problemTitle, err := rowChild.MustElement("a.table-text.text-color").Text()
 			if err != nil {
-				fmt.Println("No Title")
-				return
+				fmt.Println("Error: ", err)
+				continue
 			}
 			link := rowChild.MustElement("a.has-tooltip-bottom").MustAttribute("href")
-			fmt.Println(title, ":", *link)
-		}
+			fmt.Println(problemTitle, ":", *link)
 
+		}
 	}
+	// TODO: for each link, call the graphql leetcode API for each problem and store in DB
 }
