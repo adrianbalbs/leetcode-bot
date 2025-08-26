0ae851a31470252a058f0747bced17a9e91bec01
adrianbalbs <adrianbalbs@hotmail.com>
2025-08-26 17:16:18 +1000

    use serial for ids

--- db/migrations/20250824065200_init_db.sql
+++ db/migrations/20250824065200_init_db.sql
@@ -2,38 +2,38 @@
 -- +goose StatementBegin
 
 CREATE TABLE IF NOT EXISTS difficulties (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT UNIQUE
 );
 
 
 CREATE TABLE IF NOT EXISTS problems (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     slug TEXT UNIQUE,
     title TEXT,
     difficulty_id INT REFERENCES difficulties(id)
 );
 
 CREATE TABLE IF NOT EXISTS tags (
-      id INT PRIMARY KEY,
+      id SERIAL PRIMARY KEY,
       name TEXT UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS problem_tags (
-    problem_id INT REFERENCES problems(id),
-    tag_id INT REFERENCES tags(id),
+    problem_id SERIAL REFERENCES problems(id),
+    tag_id BIGINT REFERENCES tags(id),
     PRIMARY KEY (problem_id, tag_id)
 );
 
 CREATE TABLE IF NOT EXISTS playlists (
-    id INT PRIMARY KEY,
-    name TEXT,
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
     creator TEXT
 );
 
 CREATE TABLE IF NOT EXISTS playlist_entries (
-  playlist_id INT REFERENCES playlists(id),
-  problem_id INT REFERENCES problems(id),
+  playlist_id SERIAL REFERENCES playlists(id),
+  problem_id SERIAL REFERENCES problems(id),
   PRIMARY KEY (playlist_id, problem_id)
 );
 
